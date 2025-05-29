7cdf5ef6c58dedd25349d54a2db1430e508167f9
SeonDong <sdkim@tssni.com>
2025-05-29 17:14:10 +0900

    update

--- src/main/java/egovframework/servicename/interceptor/AllRequestInterceptor.java
+++ src/main/java/egovframework/servicename/interceptor/AllRequestInterceptor.java
@@ -1,16 +1,18 @@
 package egovframework.servicename.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class AllRequestInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("All Request Interceptor");
+        log.info("Request URI :: " + request.getRequestURI());
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
